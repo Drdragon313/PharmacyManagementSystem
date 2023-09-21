4e2f2140005cfb3311c97dd5650fd5c3df80cb00
Muhammadahmed313 <muhammadahmad742@gmail.com>
2023-09-21 11:48:31 +0500

    src changes

--- src/Iframes/Iframe.js
+++ src/Iframes/Iframe.js
@@ -10,7 +10,9 @@ const Iframe = () => {
       height="530"
       src="https://app.powerbi.com/reportEmbed?reportId=5a006d6d-811f-4132-9322-7ba6ad65b4b8&autoAuth=true&ctid=a265085c-0664-4a17-ba9a-0ae700f6b5ab"
       allowFullScreen={true}
-    ></iframe>
+    >
+      
+    </iframe>
   );
 };
 
