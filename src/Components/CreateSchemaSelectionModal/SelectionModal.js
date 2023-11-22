f268262959604f24e5c989fc02baa4367a1e3d3b
Abdur_Rafay01 <abdur.rafay@31g.co.uk>
2023-11-22 11:41:54 +0500

    styles changed

--- src/Components/CreateSchemaSelectionModal/SelectionModal.js
+++ src/Components/CreateSchemaSelectionModal/SelectionModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Style.css";
 import CustomCard from "../Card/Card";
-import { Row, Col, Modal, Space, Image } from "antd";
+import { Row, Col, Modal, Image } from "antd";
 import SchemaCreationForm from "../SchemaCreationForm/SchemaCreationForm";
 import databaseImg from "../../Assets/uploadDatabase.png";
 import manulaImg from "../../Assets/manual.png";
@@ -31,65 +31,50 @@ const SelectionModal = ({ visible, setSelectionModalVisible, tilePath }) => {
           Using this method user will upload the entire schema manually by
           adding each field and validation individually.
         </p>
-        <Row span={12}>
-          <Space size={8}>
-            <Col span={6}>
-              <CustomCard className="importfromDataBase">
-                <Space size={1} direction="vertical">
-                  <Image
-                    className="DBimg"
-                    src={databaseImg}
-                    preview={false}
-                  ></Image>
-                  <h6>Import from Database</h6>
-                  <p>
-                    This method will allow users to connect to their existence
-                    database to import their desired schema.
-                  </p>
-                </Space>
-              </CustomCard>
-            </Col>
-            <Col
-              span={6}
-              onClick={() => openSchemaCreationForm("manual")}
-              type="manual"
-            >
-              <CustomCard className="importManuallycard">
-                <Space size={1} direction="vertical">
-                  <Image
-                    className="manualImg"
-                    src={manulaImg}
-                    preview={false}
-                  ></Image>
-                  <h6>Import Manually</h6>
-                  <p>
-                    Using this method user will upload the entire schema
-                    manually by adding each field and validation individually.
-                  </p>
-                </Space>
-              </CustomCard>
-            </Col>
-            <Col
-              span={6}
-              onClick={() => openSchemaCreationForm("csv")}
-              type="csv"
-            >
-              <CustomCard className="importfromCSV">
-                <Space size={1} direction="vertical">
-                  <Image
-                    className="csvImg"
-                    src={CSVimg}
-                    preview={false}
-                  ></Image>
-                  <h6>Import from CSV</h6>
-                  <p>
-                    This method will allow users to import their schema by
-                    importing a file.
-                  </p>
-                </Space>
-              </CustomCard>
-            </Col>
-          </Space>
+        <Row
+          gutter={[16, 16]}
+          wrap={true}
+          justify={"space-evenly"}
+          align={"middle"}
+        >
+          <Col>
+            <CustomCard className="importfromDataBase">
+              <Image
+                className="DBimg"
+                src={databaseImg}
+                preview={false}
+              ></Image>
+              <h6>Import from Database</h6>
+              <p>
+                This method will allow users to connect to their existence
+                database to import their desired schema.
+              </p>
+            </CustomCard>
+          </Col>
+          <Col onClick={() => openSchemaCreationForm("manual")} type="manual">
+            <CustomCard className="importManuallycard">
+              <Image
+                className="manualImg"
+                src={manulaImg}
+                preview={false}
+              ></Image>
+              <h6>Import Manually</h6>
+              <p>
+                Using this method user will upload the entire schema manually by
+                adding each field and validation individually.
+              </p>
+            </CustomCard>
+          </Col>
+          <Col onClick={() => openSchemaCreationForm("csv")} type="csv">
+            <CustomCard className="importfromCSV">
+              <Image className="csvImg" src={CSVimg} preview={false}></Image>
+              <h6>Import from CSV</h6>
+              <p>
+                This method will allow users to import their schema by importing
+                a file.
+              </p>
+            </CustomCard>
+          </Col>
         </Row>
       </Modal>
       {isSchemaFormVisible && (
